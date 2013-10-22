93f52039e658e2962d63e1e071d5caec89655f43
leiwang80 <leiwang1688@yahoo.com>
2013-10-22 10:45:02 -0700

    add new empty project tutoring

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Tutoring::Application.config.secret_key_base = '158619ba963bd5e51fd86fd800279cba0f1e6ff48e70860587ad788f734e8a42041a6441ac4f9045ced2757ec8246789d324134c6615e39ccb4d4f542cd7cc0e'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Tutoring::Application.config.session_store :cookie_store, key: '_tutoring_session'
